ec86fd883b09d3cc422bd709aaef181c426e52f6
ketojo <131934731+ketojo@users.noreply.github.com>
2024-03-01 20:39:22 +0800

    test1
    test1

---
+++ Java/workspace/test1/src/test1/Test1.java
@@ -0,0 +1,11 @@
+package test1;//專案套件
+//中間都空一行,引用import都在這一行
+public class Test1 {
+//class 先宣告,後取名
+	public static void main(String[] args) {//期中考前程式都在這裡
+		int a = 8;
+		System.out.println(a);//print,println,
+		a = a+1;
+		System.out.println(a);
+	}
+}
